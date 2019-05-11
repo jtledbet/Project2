70ef28ba5506306df83ccd4363885bc4e77de52e
Jon <jon.ledbetter@gmail.com>
2019-05-11 10:36:37 -0400

    minor text adjustments

--- models/model.js
+++ models/model.js
@@ -1,7 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var burger = {
+var model = {
   all: function(cb) {
     orm.all("tableName", function(res) {
       cb(res);
